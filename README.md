a29534e0e32eb34977273b3b0696b60ee940fa8a
NoxFly <dorian.thivolle@gmail.com>
2023-11-26 13:21:37 +0100

    fixed audio issue, update readme, improve message repeat

--- README.md
+++ README.md
@@ -10,11 +10,13 @@ This is to reproduce the Adverse Examples attacks.
 ## Requires
 
 1. Install python3 (with pip)
-1. Install PyDub : `pip install pydub`
-1. Install ffmpeg :
-    * On Windows : `pip install ffmpeg`
+1. Install all needed packages (PyDub, gTTS, Numpy, SpeechRecognition, ffmpeg) : `pip install pydub gtts numpy SpeechRecognition ffpmeg`
+1. Install ffmpeg on OS :
+    * On Windows :
+        1. Download the zip : `https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip`
+        1. Extract to get the bin/ folder
+        1. Add the bin/ folder to the env path
     * On linux : `sudo apt-get install ffmpeg`
-1. Install Numpu : `pip install numpy`
 
 ## Execution
 
--- main.py
+++ main.py
@@ -39,13 +39,21 @@ def entryVerification(argc: int, argv: List[str]) -> tuple[AudioSegment, str]:
 
     try:
         audio = AudioSegment.from_file(argv[1])
-
         return (audio, argv[2])
 
     except:
         print("Could not load audio")
         exit(5)
 
+def getDirectoryPath(path: str) -> str:
+    return path[:path.rfind('/')] if os.path.splitext(path)[1] != '' else path
+
+# create directory recursively if it does not exist
+def ensureDirectoryExists(path: str) -> None:
+    dirpath = getDirectoryPath(path)
+
+    if not exists(dirpath):
+        os.makedirs(dirpath)
 
 # Never used
 def convertStringtoBytes(string: str) -> bytes:
@@ -59,9 +67,16 @@ def createAudioFromBytes(bytesArray: bytes):
 def createAudiofromString(msg: str) -> AudioSegment:
     language = 'fr'
     myobj = gTTS(text=msg, lang=language, slow=False)
-    myobj.save("resources/audio/tmp/tmp.mp3")
-    audio = AudioSegment.from_mp3("resources/audio/tmp/tmp.mp3")
-    os.remove("resources/audio/tmp/tmp.mp3")
+
+    filepath = "resources/audio/tmp/tmp.mp3"
+
+    ensureDirectoryExists(filepath)
+
+    myobj.save(filepath)
+    audio = AudioSegment.from_mp3(filepath)
+    os.remove(filepath)
+    os.rmdir(getDirectoryPath(filepath))
+
     return audio
 
 def readMessagefromAudio(path: str,lang = 'fr'):
@@ -71,8 +86,10 @@ def readMessagefromAudio(path: str,lang = 'fr'):
     
     with sr.AudioFile(path) as source:
         audio_file = r.record(source)
+
         try:
             print(r.recognize_google(audio_file, language=lang))
+
         except:
             print("No message found.")
             exit(7)
@@ -81,23 +98,38 @@ def readMessagefromAudio(path: str,lang = 'fr'):
 def main(argc, argv) -> None:
     (audio, message) = entryVerification(argc, argv)
 
-    positions = [1000, 5000, 9000, 13000, 17000, 21000, 25000] # toutes les 4 sec
-
     # -- Avant le 24/09S
     #bMsg = convertStringtoBytes(message)
     #hiddenAudio = createAudioFromBytes(bMsg)
 
     hiddenAudio = createAudiofromString(message)
     hiddenAudio = hiddenAudio - 22
+
+    dHidden = hiddenAudio.duration_seconds
+    dAudio = audio.duration_seconds
+
+    # delay between each hidden message
+    dmargin = 2 # seconds
+
+    # number of times the hidden message will be repeated
+    count = dAudio // (dHidden + dmargin)
+
     audio = audio - 20
-    for position in positions:
+
+    while count > 0:
+        count -= 1
+        position = count * (dHidden + dmargin) * 1000
         audio = audio.overlay(hiddenAudio, position=position)
     
     finalAudio = audio
 
-    finalAudio.export("resources/audio/results/result.wav", format="wav")
+    filepath = "resources/audio/results/result.wav"
 
-    readMessagefromAudio("resources/audio/results/result.wav")
+    ensureDirectoryExists(filepath)
+
+    finalAudio.export(filepath, format="wav")
+
+    readMessagefromAudio(filepath)
 
 #
 if __name__ == "__main__":
